bb266c37c69d84049adb4cceb029a49bac2e6fcd
Peter Finn <c16407722@mydit.ie>
2020-02-18 03:27:16 +0000

    Add files via upload

--- login.php
+++ login.php
@@ -1,11 +1,5 @@
 <?php
 $connection = new PDO("mysql:host=localhost;dbname=nwatch", "watch", "3342234Pp&^");
-echo var_dump($_POST);
-
-
-
-
-
 
 //check for cookie admin
 if($_COOKIE['LogUser'] == "admin"){
@@ -89,7 +83,7 @@ function adminConsole(){
 	echo '<tr><td><select class="bt1" name="action">
                <option value = "mod">Edit users</option>
                <option value = "del">Delete users</option>
-               <option value = "create">Add user</option>
+               <option value = "add">Add user</option>
 	     </select></td><td><input class="bt1" type="submit" value="Submit"></form></td></tr></table>';
 	
 	if($_POST["action"] == "del"){
@@ -107,7 +101,7 @@ function adminConsole(){
 
 
 	elseif($_POST["action"] == "mod"){
-		echo "<br><br><table><tr><td>Username</td><td>Firstname</td><td>Last name</td><td>House number</td><td>Password</td><td>Repeat password";
+		echo "<br><br><table><tr><td>Username</td><td>Firstname</td><td>Last name</td><td>House number</td><td>New password</td><td>Repeat password";
 		echo '<form method="post" action="login.php"></td>';
 		foreach($_POST as $key => $value)
 		{
@@ -130,11 +124,72 @@ function adminConsole(){
 		
 	}
 	elseif($_POST["action"] == "doUpdate"){
-		echo "Update complete";
+		$numUpdates=(sizeof($_POST)-1)/6;
+		$query=$conn->query("SELECT MAX(USERID) FROM USERS");
+		$num=$query->fetch();
+		
+		for ($x = 0; $x <= $num[0]; $x++) {
+			if(isset($_POST[$x."_user"])){
 
+				if($_POST[$x."_pas1"] != "" && $_POST[$x."_pas2"] != ""){
+					if($_POST[$x."_pas1"] == $_POST[$x."_pas2"]){
+				
+						$sql="UPDATE USERS SET FNAME='".$_POST[$x."_fname"]."',
+					    LNAME='".trim($_POST[$x."_lname"],"\v")."
+					    ',HOUSE='".trim($_POST[$x."_add"],"\v")."
+					    ',USERNAME='".trim($_POST[$x."_user"],"\v")."
+					    ',PASSWORD='".$_POST[$x."_pas1"]."
+					    ' WHERE USERID=".$x.";";
+					$query=$conn->query($sql);
+					echo "Updated ".$_POST[$x."_user"].".<br>";
+					}
+					else{
+						echo "Error: The passwords you entered for ".$_POST[$x."_user"]." do not match.<br>";
+					}
+				}
+				else{
+					$query=$conn->query("UPDATE USERS SET FNAME='".$_POST[$x."_fname"]."',
+					    LNAME='".$_POST[$x."_lname"]."
+					    ',HOUSE='".$_POST[$x."_add"]."
+					    ',USERNAME='".$_POST[$x."_user"]."
+					    ' WHERE USERID=".$x);
+					echo "Updated ".$_POST[$x."_user"]." with no change to the pasword<br>";
+				}
+			}
+		}
 	}
 
+	elseif($_POST["action"] == "add"){
+		echo '<br>Add user:
+		<br><br><table><tr><td>Username</td><td>Firstname</td><td>Last name</td><td>House number</td><td>New password</td><td>Repeat password
+		<form method="post" action="login.php"></td><tr>
+		<td><input type="text" name="user"></td>
+		<td><input type="text" name="fname"></td>
+		<td><input type="text" name="lname"></td>
+		<td><input type="text" name="add"></td>
+		<td><input type="password" name="pas1"></td>
+		<td><input type="password" name="pas2"></td></tr>
+		<tr><td>
+		<input type="hidden" id="action2" name="action" value="doAdd">
+		<input type="submit" class="bt1" value="Update"></td></tr></form></table>';
 
+
+	}
+	elseif($_POST["action"] == "add"){
+
+	}
+	//fix php crap
+	$query=$conn->query("
+	UPDATE USERS SET USERNAME = TRIM(CHAR(9) FROM TRIM(USERNAME));
+	UPDATE USERS SET PASSWORD = TRIM(CHAR(9) FROM TRIM(PASSWORD));
+	UPDATE USERS SET FNAME = TRIM(CHAR(9) FROM TRIM(FNAME));
+	UPDATE USERS SET LNAME = TRIM(CHAR(9) FROM TRIM(LNAME));
+	UPDATE USERS SET HOUSE = TRIM(CHAR(9) FROM TRIM(HOUSE));
+	UPDATE USERS SET USERNAME = TRIM(CHAR(10) FROM TRIM(USERNAME));
+	UPDATE USERS SET PASSWORD = TRIM(CHAR(10) FROM TRIM(PASSWORD));
+	UPDATE USERS SET FNAME = TRIM(CHAR(10) FROM TRIM(FNAME));
+	UPDATE USERS SET LNAME = TRIM(CHAR(10) FROM TRIM(LNAME));
+	UPDATE USERS SET HOUSE = TRIM(CHAR(10) FROM TRIM(HOUSE));");
 
 }
 
