6bd426136b7c595affe389beee24ea64d730c626
Peter Finn <c16407722@mydit.ie>
2020-03-16 14:20:49 +0000

    Delete dbConnect.php

--- dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-$conn=$connection=new PDO("mysql:host=localhost;dbname=nwatch", "watch", "3342234Pp&^");
-}
-catch(PDOException $e)
-    {
-    echo "Error: " . $e->getMessage();
-    }
-?>
