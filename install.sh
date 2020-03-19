9efce0d27e2ea80be6f116e1078734195565f0b7
Peter Finn <c16407722@mydit.ie>
2020-03-19 23:56:36 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@ then
 	echo "The install needs root permissions"
 	exit 0
 fi
+cp -r installFiles/nwatch.conf /etc/nwatch.conf
+. /etc/nwatch.conf
 
 sudo rm -r /etc/nwatch
 sudo mkdir /etc/nwatch
 sudo mkdir /var/nwatch
 sudo mkdir /srv/http/nwatch
-. /etc/nwatch.conf
 sudo touch $logDir
 sudo chmod 777 $logDir
 sudo chmod 777 -R /etc/nwatch
 sudo chmod 777 -R /var/nwatch
 
 pip install pycurl
-sudo pacman -S motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl
+sudo pacman -S unzip wget motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl
 wget https://github.com/umlaeute/v4l2loopback/archive/master.zip
 unzip master.zip
 cd v4l2loopback-master
@@ -24,7 +25,6 @@ sudo make install
 sudo modprobe v4l2loopback
 
 cp -r camera /var/nwatch
-cp -r installFiles/nwatch.conf /etc/nwatch.conf
 cp -r installFiles/php.ini /etc/php/php.ini
 cp -r installFiles/motion.conf /etc/motion
 cp -r wifi /var/nwatch
