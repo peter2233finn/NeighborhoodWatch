44b02495e203f2e77ab0f8ea082999b2a6499d0b
Peter Finn <c16407722@mydit.ie>
2020-03-23 20:55:06 +0000

    Add files via upload

--- camera/startCam.sh
+++ camera/startCam.sh
@@ -1,4 +1,3 @@
 . /etc/nwatch.conf
-
-ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 -loglevel 2
+sudo ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
 
--- install.sh
+++ install.sh
@@ -9,14 +9,13 @@ cp -r installFiles/nwatch.conf /etc/nwatch.conf
 sudo rm -r /etc/nwatch
 sudo mkdir /etc/nwatch
 sudo mkdir /var/nwatch
-sudo mkdir /srv/http/nwatch
 sudo touch $logDir
 sudo chmod 777 $logDir
 sudo chmod 777 -R /etc/nwatch
 sudo chmod 777 -R /var/nwatch
 
-
-sudo pacman -S unzip wget motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl make
+sudo pacman -S base-devel linux-raspberrypi4-headers dkms
+sudo pacman -S unzip wget motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl make  
 systemctl enable mysqld
 systemctl enable httpd
 cp -r nwatchHttp/* /srv/http/
@@ -25,15 +24,68 @@ cp -r nwatchHttp/* /srv/http/
 wget http://standards-oui.ieee.org/oui.txt -P /etc/nwatch
 pip install pycurl
 
-wget https://github.com/umlaeute/v4l2loopback/archive/master.zip
-unzip master.zip
-cd v4l2loopback-master
-make
-sudo make install
-sudo modprobe v4l2loopback
+#wget https://github.com/umlaeute/v4l2loopback/archive/master.zip
+#unzip master.zip
+#cd v4l2loopback-master
+#make
+#sudo make install
+#sudo modprobe v4l2loopback
 
 cp -r camera /var/nwatch
 cp -r installFiles/php.ini /etc/php/php.ini
 cp -r installFiles/motion.conf /etc/motion
 cp -r wifi /var/nwatch
-cp -r nwatchHttp/* /srv/http/nwatch
+cp -r nwatchHttp/* /srv/http
+
+echo "Adding user nwatch"
+mkdir /home/nwatch
+useradd nwatch -d /home/nwatch/
+echo "Enter the password for the user nwatch"
+passwd nwatch
+
+
+sudo cp installFiles/httpd.conf /etc/httpd/conf/httpd.conf
+sudo cp installFiles/php.ini /etc/php/php.ini
+
+sudo chmod 777 /etc/php/php.ini
+echo extension=pdo.so >> /etc/php/php.ini
+echo extension=pdo_mysql >> /etc/php/php.ini
+echo extension=pdo_pgsql >> /etc/php/php.ini
+echo extension=pdo_sqlite >> /etc/php/php.ini
+sudo chmod 644 /etc/php/php.ini
+
+# Enable the database
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+mysql_secure_installation
+
+echo "Enter password for mysql root user to install the database:"
+read dbPass
+
+mysql -u root -p"$dbPass" -e"drop user watch@localhost;drop database nwatch;"
+mysql -u root -p"$dbPass" < installFiles/nwatch.sql
+
+# Install v4l2loopback for the camera
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Sy
+yay -S v4l2loopback-dkms
+sudo dkms autoinstall
+
+sudo reboot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
