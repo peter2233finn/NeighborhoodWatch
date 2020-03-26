2caf05060a505601d533e0014a01aa5ecfb4c4f2
Peter Finn <c16407722@mydit.ie>
2020-03-26 22:49:08 +0000

    Update alpr.sh
    Changed to support camera.

--- camera/alpr.sh
+++ camera/alpr.sh
@@ -1,23 +1,25 @@
 . /etc/nwatch.conf
 while true
 do
-	for i in /var/nwatch/camera/*;
-	do
-		if [[ ${i: -3} == "mkv" ]]
-		then
-			time=${i:: -4} 
-			plate=""
-#	do alpr stuff here
-# 	put into varible plate
+        for i in /var/nwatch/camera/*;
+        do
+                if [[ ${i: -3} == "mkv" ]]
+                then
+                        time=$(echo ${i:: -4} | tr "/" " " | rev | awk '{print $1 " " $2}'| rev)
+                        fileName=$(echo $i | tr "/" " " | rev | awk '{print $1 " " $2}'| rev)
+                        plate=""
+#       do alpr stuff here
+#       put into varible plate
 
-			if [[ $plate == "" ]]
-			then
-				mysql -u $dbUser -p"$dbPassword" nwatch -e"INSERT INTO CAMEVENT(VIDDIR,TIME) VALUES('camEvents/$i','$time')"
-			else
-				mysql -u $dbUser -p"$dbPassword" nwatch -e"INSERT INTO CAMEVENT(PLATE,VIDDIR,TIME) VALUES('$plate','/var/nwatch/$i','$time')"
-			fi
-			mv "$i" /srv/http/nwatch/camEvents/
-			fi
-	done
-	sleep 10
+                        if [[ $plate == "" ]]
+                        then
+                                mysql -u $dbUser -p"$dbPassword" nwatch -e"INSERT INTO CAMEVENT(VIDDIR,TIME) VALUES('/camEvents/$fileName','$time')"
+                        else
+                                mysql -u $dbUser -p"$dbPassword" nwatch -e"INSERT INTO CAMEVENT(PLATE,VIDDIR,TIME) VALUES('$plate','/camEvent/$fileName','$time')"
+                        fi
+
+                        mv "$i" /srv/http/camEvents/
+                        fi
+        done
+        sleep 10
 done
