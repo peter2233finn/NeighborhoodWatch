0bc1ccce419a488c5eee2ea1af575002f04dda7b
Peter Finn <c16407722@mydit.ie>
2020-03-31 14:27:33 +0100

    Update update.php

--- nwatchHttp/update.php
+++ nwatchHttp/update.php
@@ -1,59 +1,60 @@
-<html>
-<link rel="stylesheet" href="style.css">
-<body>
 <?php
 include 'auth.php';
 include 'dbConnect.php';
 
+if( isset($_POST['delEvent']) ){
+        include 'adminAuth.php';
+        $h = $conn->query("delete from CAMEVENT where VIDID = '". $_POST['delEvent'] ."'");
+        $msg.="Event has been deleted";
+        setcookie("msg","Event has been deleted.");
+        header("Location: adminMain.php");
+        die();
+}
 if( isset($_POST['cur']) ){
-	$pwd = $conn->query("select password from USERS WHERE USERID = '". $_COOKIE['LogUser'] ."'");
-	
-	while ($row = $pwd->fetch()) {
-			$password= $row['password'];
-		}
-	if($password === $_POST["cur"]){
-		
-		if($_POST["new1"] === $_POST["new2"]){
+        $pwd = $conn->query("select password from USERS WHERE USERID = '". $_COOKIE['LogUser'] ."'");
 
-			if($_POST["new1"]==NULL){
-				$msg="Please enter something for the new password";
-			}
-			else{
-				$updated = $conn->query("UPDATE USERS SET PASSWORD='".$_POST["new1"]."' WHERE USERID = ". $_COOKIE['LogUser']);
-			
-				$msg="Your password was sucessfully updated";
-			}
-		}
-		else{
-			$msg="The passwords you entered do not match.";
-		}
+        while ($row = $pwd->fetch()) {
+                        $password= $row['password'];
+                }
+        if($password === $_POST["cur"]){
+
+                if($_POST["new1"] === $_POST["new2"]){
+
+                        if($_POST["new1"]==NULL){
+                                $msg="Please enter something for the new password";
+                        }
+                        else{
+                                $updated = $conn->query("UPDATE USERS SET PASSWORD='".$_POST["new1"]."' WHERE USERID = ". $_COOKIE['LogUser']);
+
+                                $msg="Your password was sucessfully updated";
+                        }
+                }
+                else{
+                        $msg="The passwords you entered do not match.";
+                }
 
 
-	}
-	else{
-		$msg="The password you entered is wrong. Please try again.";
-	}
+        }
+        else{
+                $msg="The password you entered is wrong. Please try again.";
+        }
 
 }
 else{
 
-	$blackList=explode("\n", $_POST["bl"]);
-	$whiteList=explode("\n", $_POST["bl"]);
-//	foreach ($array as $item) {
-//		echo "ZZZ" . $item;
-//	}
-
-	//echo $_POST["bl"];
-
-
-
+        $blackList=explode("\n", $_POST["bl"]);
+        $whiteList=explode("\n", $_POST["wl"]);
+        foreach ($whiteList as $item) {
+                $conn = $connection->query("INSERT INTO LISTS(WLIST, USERID) VALUES('".$item."','". $_COOKIE['LogUser']."')");
+        }
+        foreach ($blackList as $item) {
+                $conn = $connection->query("INSERT INTO LISTS(BLIST, USERID) VALUES('".$item."','". $_COOKIE['LogUser']."')");
+        }
+        setcookie("msg",$msg);
+        header("Location: main.php");
+	die();
 }
 
-setcookie("msg",$msg);
-
-header("Location: main.php");
+header("Location: index.php");
 
 ?>
-
-</body>
-</html>
