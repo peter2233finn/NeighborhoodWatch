ddc44600baecdd458268ce807d3fd580c91a6fd9
Peter Finn <c16407722@mydit.ie>
2020-03-31 15:46:08 +0100

    Doesnt spam up terminal anymore

--- camera/startCam.sh
+++ camera/startCam.sh
@@ -1,3 +1,2 @@
 . /etc/nwatch.conf
-sudo ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
-
+sudo ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 2> /dev/null > /dev/null
