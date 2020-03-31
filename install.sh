719718b962c0bd937edab9b5e4cc0c45b20d2cf5
Peter Finn <c16407722@mydit.ie>
2020-03-31 18:04:29 +0100

    Added lsof to installer

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo chmod 777 /srv/http/camEvents/
 echo "Installing dependancies"
 
 sudo pacman -S base-devel usbutils linux-raspberrypi-headers dkms jq aircrack-ng tcpdump #linux-headers
-sudo pacman -S unzip wget motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl make  
+sudo pacman -S unzip wget motion mysql php php-sqlite php-apache php-pgsql ffmpeg curl make lsof
 systemctl enable mysqld
 systemctl enable httpd
 cp -r nwatchHttp/* /srv/http/
