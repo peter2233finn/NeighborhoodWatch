27e4e944554f92e05465f1edbe83aa6052b34cfd
Peter Finn <c16407722@mydit.ie>
2020-03-31 13:18:44 +0100

    Delete startCam.sh

--- nwatchHttp/camera/startCam.sh
+++
@@ -1,6 +0,0 @@
-. /etc/nwatch.conf
-sudo rmmod v4l2loopback
-sudo modprobe v4l2loopback
-ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 -loglevel 2 2>&1
-sudo chmod 777 /dev/video0
-motion >$logDir 2>&1
