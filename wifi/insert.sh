6f8237fb944f050a09b893da607ae4b52db1c083
Peter Finn <c16407722@mydit.ie>
2020-04-04 23:28:23 +0100

    Hops channels with fork to nwatch.process
    When service stopped the hopping will also be stopped using the PID. No fork kill previously used

--- wifi/insert.sh
+++ wifi/insert.sh
@@ -1,24 +1,21 @@
 . /etc/nwatch.conf
 
-
 interface="$(ifconfig | grep mon | head -n 1 | awk '{print $1}' | tr ":" " "| xargs)"
-echo $interface
 
 channel_hop() {
 
-	IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
-	IEEE80211bg_intl="$IEEE80211b 12 13 14"
-	IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
-	IEEE80211bga="$IEEE80211bg $IEEE80211a"
-	IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
+        IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
+        IEEE80211bg_intl="$IEEE80211b 12 13 14"
+        IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
+        IEEE80211bga="$IEEE80211bg $IEEE80211a"
+        IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
 
-	while true ; do
-		for CHAN in $IEEE80211bg ; do
-			# echo "switching $IFACE to channel $CHAN"
-			sudo iwconfig $IFACE channel $CHAN
-			sleep 2
-		done
-	done
+        while true ; do
+                for CHAN in $IEEE80211bg ; do
+                        sudo iwconfig $interface channel $CHAN
+                        sleep 1.5
+                done
+        done
 }
 
 
@@ -30,11 +27,11 @@ dbInput="INSERT INTO SCANNERLIMBO(MAC, ESSID) VALUES('$MAC','$(echo $SSID|xargs)
 mysql --user="$dbUser" --password="$dbPassword" --database="$dbName" --execute="$dbInput"
 
 }
-
+channel_hop &
+echo $! >> /tmp/nwatch.process
 #replace test with command and remove function test
 sudo tcpdump -l -I -i "$interface" -e -s 256 type mgt subtype probe-req | awk -f /var/nwatch/wifi/parse-tcpdump.awk | while IFS= read -r output
 do
-	insert $output 
-	echo "[INFO]: Found iprobe: $output" >> $logDir
+        insert $output 
+        echo "[INFO]: Found iprobe: $output" >> $logDir
 done
-
