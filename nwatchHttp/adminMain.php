0b690327e69bdb668ade27b141f1a113fd9ecc49
Peter Finn <c16407722@mydit.ie>
2020-04-07 05:00:36 +0100

    Added log viewing function

--- nwatchHttp/adminMain.php
+++ nwatchHttp/adminMain.php
@@ -1,12 +1,62 @@
 <html>                                                                                                                                                                                                                                    
 <link rel="stylesheet" href="style.css">                                                                                                                                                                                                  
+<body>
+<form action="/showLog.php" method="post">
+  <label for="rows">Log lines</label>
+  <input type="number" name="rows" value="500" style="width: 10%;" min="0">
+  <label for="cols">Display in columns</label>
+  <input type="number" name="cols" value="3" style="width: 10%;" min="0">
+  <input type="submit" class="bt1" value="Update">
+</form>
+<?php
+include 'adminAuth.php';
+if(isset($_POST['rows'])){
+        $rowNum=intval($_POST['cols']);
+        $lineNum=intval($_POST['rows']);
+
+}
+else{
+        $rowNum=1;
+        $lineNum=1000;
+}
+$log = file("nwatch.log") or die("Error: Unable to open the log file<br>
+        There should be a symbolic link between the log directory and the file nwatch.log in the server folder");
+echo "<font size=8>Neighbourhood watch system log</font><br><br><center><table>";
+
+$newLine=0;
+$linesToPrint=count($log)*$lineNum;
+for ($i = count($log)-1; $i >= count($log)-$lineNum; $i--) {
+        echo "<td>";
+        echo $log[$i] . "</td>";
+        if($newLine%$rowNum == ($rowNum-1)){
+                echo "</tr>";
+        }
+        if($log[$i] == ""){
+                break;
+        }
+        $newLine++;
+}
+if(count($log)==0){
+        echo '<tr><td>There is noting in the log file</td></tr>';
+}
+echo "</table></center>";
+
+fclose($log);
+
+?>
+</body>
+</html>
+cat: cat: No such file or directory
+<html>                                                                                                                                                                                                                                    
+<link rel="stylesheet" href="style.css">                                                                                                                                                                                                  
 <body>                                                                                                                                                                                                                                    
    <table class="mainTable">                                                                                                                                                                                                              
    <tr>                                                                                                                                                                                                                                   
            <td>                                                                                                                                                                                                                           
                    <p class="bold">Events</p>                                                                                                                                                                                             
                    <?php                                                                                                                                                                                                                  
-                           include "dbConnect.php";                                                                                                                                                                                       
+                           include 'dbConnect.php';                                                                                                                                                                                       
+                           include 'adminAuth.php';                                                                                                                                                                                       
                            echo "<table class='carFeedTable'>                                                                                                                                                                             
                            <tr><td>Delete</td><th>Time</th><th>Regristration plate</th><th>Open event</th></tr>";                                                                                                                                        
                            $conn = $connection->query("select VIDID,VIDDIR,PLATE,TIME from CAMEVENT ORDER BY TIME DESC");                                                                                                                 
@@ -74,6 +124,18 @@ New password <input type="password" name="new2"><br>
                            </td>
                            </tr>
                            </table>
+<table>
+<tr><td>
+<form action="/showLog.php" method="post">
+        <input class="bt1" type="submit" value="Show log">
+</form></td><td>
+<form action="/showLog.php" method="post">
+        <input class="bt1" type="submit" value="Start nwatch">
+</form></td><td>
+<form action="/showLog.php" method="post">
+        <input class="bt1" type="submit" value="Stop nwatch">
+</form></td></tr>
+</table>
    <?php
            echo $_COOKIE["msg"];
            unset($_COOKIE["msg"]);
@@ -85,5 +147,3 @@ New password <input type="password" name="new2"><br>
    </table>
 </body>
 </html>
-
-</form>
