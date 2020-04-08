bceb1c4471f63f49dc381a9f55001b3f42d116e6
Peter Finn <c16407722@mydit.ie>
2020-04-08 23:28:58 +0100

    Added fail safe for storage error

--- camera/alpr.sh
+++ camera/alpr.sh
@@ -5,11 +5,22 @@ do
         do
                 if [[ ${i: -3} == "mkv" ]]
                 then
+                        if [[ $storage != "" ]]
+                                then
+                                if [[ ! -d "$storage/" ]]
+                                then
+                                        echo "[ERR]: The directory to store videos ($storage) has failed. Exiting. Watchdog will attempt again in 30 seconds"
+                                        echo "[ERR]: The videos have not been deleted and are stored in /var/nwatch/camera"
+                                        exit
+                                fi
+                        fi
+
+
                         time=$(echo ${i:: -4} | tr "/" " " | rev | awk '{print $1 " " $2}'| rev)
                         fileName=$(echo $i | tr "/" " " | rev | awk '{print $1 " " $2}'| rev)
                         plate=""
-#       do alpr stuff here
-#       put into varible plate
+#       ALPR to be peformed here if you want to add it.
+#       put the lisence plate into varible plate
 
                         if [[ $plate == "" ]]
                         then
@@ -18,7 +29,7 @@ do
                                 mysql -u $dbUser -p"$dbPassword" nwatch -e"INSERT INTO CAMEVENT(PLATE,VIDDIR,TIME) VALUES('$plate','/camEvent/$fileName','$time')"
                         fi
 
-                        mv "$i" /srv/http/camEvents/
+                        mv "$i" "/srv/http/camEvents"
                         fi
         done
         sleep 10
