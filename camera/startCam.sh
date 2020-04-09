f0e8ee2bb485c4e448fbe6c206e4603aac099cf2
Peter Finn <c16407722@mydit.ie>
2020-04-09 05:18:03 +0100

    Added functionality for user and pass

--- camera/startCam.sh
+++ camera/startCam.sh
@@ -1,2 +1,13 @@
 . /etc/nwatch.conf
+httpType=$camURL
+if [[ $httpType == *"https"*  ]]
+then
+                http="https://"
+        else
+                        http="http://"
+fi
+camURL=$(echo $camURL | sed 's/https:\/\///')
+camURL=$(echo $camURL | sed 's/http:\/\///')
+camURL="$http""$camUser"":""$camPass""@""$camURL"
+
 sudo ffmpeg -re -i "$camURL" -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 2> /dev/null > /dev/null
