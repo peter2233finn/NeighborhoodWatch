3e9e1e23dece600f1b776034989a61ef1ff464c4
Peter Finn <c16407722@mydit.ie>
2020-04-12 03:29:01 +0100

    Added exception for ' in query

--- wifi/insert.sh
+++ wifi/insert.sh
@@ -4,18 +4,18 @@ interface="$(ifconfig | grep mon | head -n 1 | awk '{print $1}' | tr ":" " "| xa
 
 channel_hop() {
 
-        IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
-        IEEE80211bg_intl="$IEEE80211b 12 13 14"
-        IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
-        IEEE80211bga="$IEEE80211bg $IEEE80211a"
-        IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
+	IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
+	IEEE80211bg_intl="$IEEE80211b 12 13 14"
+	IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
+	IEEE80211bga="$IEEE80211bg $IEEE80211a"
+	IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
 
-        while true ; do
-                for CHAN in $IEEE80211bg ; do
-                        sudo iwconfig $interface channel $CHAN
-                        sleep 1.5
-                done
-        done
+	while true ; do
+		for CHAN in $IEEE80211bg ; do
+			sudo iwconfig $interface channel $CHAN
+			sleep 1.5
+		done
+	done
 }
 
 
@@ -23,7 +23,7 @@ function insert {
 MAC="$3"
 SSID="$4 $5 $6 $7 $8 $9"
 
-dbInput="INSERT INTO SCANNERLIMBO(MAC, ESSID) VALUES('$MAC','$(echo $SSID|xargs)');"
+dbInput="INSERT INTO SCANNERLIMBO(MAC, ESSID) VALUES('$MAC','$(echo $SSID|xargs| sed "s/'/''/g")');"
 mysql --user="$dbUser" --password="$dbPassword" --database="$dbName" --execute="$dbInput"
 
 }
@@ -32,6 +32,6 @@ echo $! >> /tmp/nwatch
 #replace test with command and remove function test
 sudo tcpdump -l -I -i "$interface" -e -s 256 type mgt subtype probe-req | awk -f /var/nwatch/wifi/parse-tcpdump.awk | while IFS= read -r output
 do
-        insert $output 
-        echo "[INFO]: Found iprobe: $output" >> $logDir
+	insert $output 
+	echo "[INFO]: Found iprobe: $output" >> $logDir
 done
