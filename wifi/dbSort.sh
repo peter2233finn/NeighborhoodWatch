85ca82e27792be088be460784ab7615f06bb703e
Peter Finn <c16407722@mydit.ie>
2020-04-14 07:33:54 +0100

    Added commenting

--- wifi/dbSort.sh
+++ wifi/dbSort.sh
@@ -1,7 +1,10 @@
 . /etc/nwatch.conf
 set -f # disable globbing
 
+# videoDel delets videos that are removed from database by admin
 videoDel=0
+
+# Start infinite loop
 while true
 do
 	wiggleStatus=1
@@ -9,6 +12,7 @@ do
 	x=0
 	while read line
 	do 
+		# This checks if wiggle bans user. This can happen from too manyqueries today
 		set=1
 		if [[ $(cat /tmp/nwatch.process | grep limitReached|head -n 1) == "limitReached" ]]
 		then
@@ -16,7 +20,7 @@ do
 			sleep 30m
 			echo "" > /tmp/nwatch.process
 			
-			# makes sure its not the first row, which holds no relivand data
+			# makes sure its not the first row, which holds no relivant data
 		elif (( $x != 0 ))
 		then
 			# query holds the SSID
@@ -47,6 +51,7 @@ do
 				y=0
 				mac=$(echo ${line[@]} | awk '{print $2}')
 				# runs through every results recieved from wiggle
+				# This is all the data recieved from wigle in Json format
 				while [[ $(echo $results | jq ".results[$y].trilat")  != "null" ]]
 				do
 					echo $results | jq ".results[$y].encryption"
@@ -69,11 +74,12 @@ do
 				array+=("$line")
 
 			fi
+			# This formats the data for the database
 			essid=$((echo ${line[@]} | awk '{print $5" "$6" "$7" "$8" "$9" "$10}') | xargs | tr ' ' '+')
 			time=$(echo $line | awk '{print $3 " " $4}')
-			#echo $time
 			mac=$(echo $line | awk '{print $2}')
 			# find mac vendor using oui.txt
+			# It checks for buth versions of MAC oui data
 			macVendor=$(grep -i "$(echo $mac | cut -c 1-11 | sed "s/://g")" /etc/nwatch/oui.txt -m 1)
 			if [[ $macVendor=="" ]]
 			then
@@ -93,6 +99,7 @@ do
 			then
 				mysql -u $dbUser -p"$dbPassword" nwatch -e"insert into BEACON(VENDOR,MAC,ESSID,TIME) VALUES('$macVendor','$mac','$(echo $essid| sed "s/'/''/g")','$time');"
 			else
+				# This is triggered if the ESSID is blacklisted
 				if [[ "$ALERT" != "" ]]
 				then
 					sh /var/nwatch/blacklistAlert.sh "$essid" "$mac" "$macVendor" "$time"
@@ -104,6 +111,7 @@ do
 	((x++))
 	done < <(mysql -u $dbUser -p"$dbPassword" nwatch -e"select ID, MAC, TIME, ESSID from SCANNERLIMBO")
 
+	# Every 5 loops, delete the videos not in the database
 	dodel=$(($videoDel % 5))
 		if (( $dodel == 0))
 		then
@@ -117,7 +125,7 @@ do
 
 			while read delete; 
 			do
-				rm "/srv/http/camEvents/$(echo $delete |egrep -v "camEvents|VIDDIR";)" 2>&1 >/dev/null | grep -v "Is a directory"
+				rm "/srv/http/camEvents/$(echo $delete |egrep -v "camEvents|VIDDIR";)" 2>&1 >/dev/null | grep -v "Is a directory"	
 			done<<<$(diff --changed-group-format='%<%>' --unchanged-group-format='' /tmp/nwatch-notDelete /tmp/nwatch-possDelete)
 			rm /tmp/nwatch-notDelete
 			rm /tmp/nwatch-possDelete
