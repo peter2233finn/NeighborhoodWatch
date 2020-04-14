868d39bec2586d5685465c29d38752fb796c4942
Peter Finn <c16407722@mydit.ie>
2020-04-14 07:16:58 +0100

    Update showLog.php

--- nwatchHttp/showLog.php
+++ nwatchHttp/showLog.php
@@ -11,33 +11,33 @@
 <?php
 include 'adminAuth.php';
 if(isset($_POST['rows'])){
-        $rowNum=intval($_POST['cols']);
-        $lineNum=intval($_POST['rows']);
+	$rowNum=intval($_POST['cols']);
+	$lineNum=intval($_POST['rows']);
 
 }
 else{
-        $rowNum=1;
-        $lineNum=1000;
+	$rowNum=1;
+	$lineNum=1000;
 }
 $log = file("nwatch.log") or die("Error: Unable to open the log file<br>
-        There should be a symbolic link between the log directory and the file nwatch.log in the server folder");
+	There should be a symbolic link between the log directory and the file nwatch.log in the server folder");
 echo "<font size=8>Neighbourhood watch system log</font><br><br><center><table>";
 
 $newLine=0;
 $linesToPrint=count($log)*$lineNum;
 for ($i = count($log)-1; $i >= count($log)-$lineNum; $i--) {
-        echo "<td>";
-        echo $log[$i] . "</td>";
-        if($newLine%$rowNum == ($rowNum-1)){
-                echo "</tr>";
-        }
-        if($log[$i] == ""){
-                break;
-        }
-        $newLine++;
+	echo "<td>";
+	echo $log[$i] . "</td>";
+	if($newLine%$rowNum == ($rowNum-1)){
+		echo "</tr>";
+	}
+	if($log[$i] == ""){
+		break;
+	}
+	$newLine++;
 }
 if(count($log)==0){
-        echo '<tr><td>There is noting in the log file</td></tr>';
+	echo '<tr><td>There is noting in the log file</td></tr>';
 }
 echo "</table></center>";
 
